cb28d3a08f6e025aed6381baa5df100d2127438e
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-07-07 14:02:14 +0900

    fix: yml#1

--- scripts/generate-changelog.cjs
+++ scripts/generate-changelog.cjs
@@ -1,26 +1,29 @@
+const { default: readChangesets } = require("@changesets/read");
+const { default: getReleasePlan } = require("@changesets/get-release-plan");
 const { getReleaseLine } = require("@changesets/changelog-github");
 const fs = require("fs");
 const path = require("path");
-const { default: getReleasePlan } = require("@changesets/get-release-plan");
 
-getReleasePlan()
-  .then(async (releasePlan) => {
-    const githubOptions = {
-      repo: "changesets-test",
-      user: "Jason-Jeong-Dev",
-    };
+(async () => {
+  const cwd = process.cwd();
+  const changesets = await readChangesets(cwd);
+  const releasePlan = await getReleasePlan(changesets);
 
-    let changelogText = "";
-    for (const release of releasePlan.releases) {
-      if (release.type === "none") continue;
+  const githubOptions = {
+    repo: "changesets-test",
+    user: "Jason-Jeong-Dev",
+  };
 
-      const releaseEntries = release.changelogEntries;
-      changelogText += await getReleaseLine(release, releaseEntries, githubOptions);
-      changelogText += "\n\n";
-    }
-    fs.writeFileSync("CHANGELOG.md", changelogText, { flag: "a+" });
-  })
-  .catch((error) => {
-    console.error("Error generating changelog:", error);
-    process.exit(1);
-  });
+  let changelogText = "";
+  for (const release of releasePlan.releases) {
+    if (release.type === "none") continue;
+
+    const releaseEntries = release.changelogEntries;
+    changelogText += await getReleaseLine(release, releaseEntries, githubOptions);
+    changelogText += "\n\n";
+  }
+  fs.writeFileSync("CHANGELOG.md", changelogText, { flag: "a+" });
+})().catch((error) => {
+  console.error("Error generating changelog:", error);
+  process.exit(1);
+});
